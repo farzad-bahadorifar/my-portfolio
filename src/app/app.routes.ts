d953eae11769293a358b617a16e963d1fbff46db
Farzad Bahadorifar <34229837+f6rz9d@users.noreply.github.com>
2024-05-11 22:44:25 +0330

    feat(app-component): add navbar links and route to the links.
    Signed-off-by: Farzad Bahadorifar <34229837+f6rz9d@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'skills', component: SkillsComponent },
+];
